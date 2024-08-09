1b4e15d796d3c6b7c4b68ffa725c18d55975f389
Dylan Pereira <dylanperira0204@gmail.com>
2024-08-08 22:00:23 -0600

    ay mi gatito miau miau

--- ET/ETTrabajador.cs
+++ ET/ETTrabajador.cs
@@ -8,5 +8,16 @@ namespace ET
 {
     internal class ETTrabajador
     {
+        private int _IDTrabajador;
+        private string _Nombre;
+        private string _Cedula;
+        private string _Correo;
+        private string _Telefono;
+
+        public int IDTrabajador { get => _IDTrabajador; set => _IDTrabajador = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Cedula { get => _Cedula; set => _Cedula = value; }
+        public string Correo { get => _Correo; set => _Correo = value; }
+        public string Telefono { get => _Telefono; set => _Telefono = value; }
     }
 }
