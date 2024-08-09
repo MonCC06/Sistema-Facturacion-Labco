aab4b16adda8b1930e61df5d8a5b8acdac67264b
DavidVilanova <dvilanova001@gmail.com>
2024-08-08 21:21:04 -0600

    ET Detalle

--- ET/ETDetalle.cs
+++ ET/ETDetalle.cs
@@ -8,5 +8,17 @@ namespace ET
 {
     internal class ETDetalle
     {
+        private int _IDDetalle;
+        private int _IDServicio;
+        private int _IDProducto;
+        private float _Monto;
+        private int _Cantidad;
+     
+
+        public int IDDetalle { get => _IDDetalle; set => _IDDetalle = value; }
+        public int IDServicio { get => _IDServicio; set => _IDServicio = value; }
+        public int IDProducto { get => _IDProducto; set => _IDProducto = value; }
+        public float Monto { get => _Monto; set => _Monto = value; }
+        public int Cantidad { get => _Cantidad; set => _Cantidad = value; }
     }
 }
