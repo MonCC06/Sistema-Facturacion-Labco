e0e4d3d3feb0a18a71cd8dd38c385e16b6db521f
santiago arce godinez <arcegodinezs@gmail.com>
2024-08-09 16:07:50 -0600

    Actualizacion

---
+++ GUI/FrmInicio.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GUI
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbpPrincipal = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TbpPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TbpPrincipal
+            // 
+            this.TbpPrincipal.Controls.Add(this.tabPage1);
+            this.TbpPrincipal.Controls.Add(this.tabPage2);
+            this.TbpPrincipal.Location = new System.Drawing.Point(3, 3);
+            this.TbpPrincipal.Name = "TbpPrincipal";
+            this.TbpPrincipal.SelectedIndex = 0;
+            this.TbpPrincipal.Size = new System.Drawing.Size(891, 451);
+            this.TbpPrincipal.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(883, 425);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(874, 166);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 568);
+            this.Controls.Add(this.TbpPrincipal);
+            this.Name = "FrmInicio";
+            this.Text = "Inicio";
+            this.TbpPrincipal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TbpPrincipal;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
