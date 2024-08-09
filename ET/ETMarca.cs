0cc24159bde51ef7b33201deb4ff282f31458b03
angel vargas <avargasmy222@outlook.com>
2024-08-09 12:59:43 -0600

    EtEntidad

--- ET/ETMarca.cs
+++ ET/ETMarca.cs
@@ -8,5 +8,12 @@ namespace ET
 {
     internal class ETMarca
     {
+        private int _IDMarca;
+        private string _Name;
+        private bool _Estado;
+
+        public int IDMarca { get => _IDMarca; set => _IDMarca = value; }
+        public string Name { get => _Name; set => _Name = value; }
+        public bool Estado { get => _Estado; set => _Estado = value; }
     }
 }
