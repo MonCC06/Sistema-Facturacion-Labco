12590b8638a20351915e1b62d1b731553451581d
Juan Pablo Monge Guzmán <pabloguzman121989@gmail.com>
2024-08-09 13:51:04 -0600

    Se cambio las clase de cliente

--- DAL/DALCliente.cs
+++ DAL/DALCliente.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ET;
 
 namespace DAL
 {
-    internal class DALCliente
+    public class DALCliente
     {
+
     }
 }
