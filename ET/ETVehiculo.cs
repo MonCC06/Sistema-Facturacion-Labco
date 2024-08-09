85f334c4c20f37d80f1dfa0e45fa656ce28a2cd4
DavidVilanova <dvilanova001@gmail.com>
2024-08-08 21:21:04 -0600

    ETVEHICULO

--- ET/ETVehiculo.cs
+++ ET/ETVehiculo.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ET
 {
-    internal class ETVehiculo
+    public class ETVehiculo
     {
+        private int _IDVehiculo;
+        private int _IDCliente;
+        private int _IDMarca;
+        private string _Modelo;
+        private string _Anno;
+        private string _VIN;
+        private string _Placa;
+        private Boolean _TipodeDistancia;
+        private float _DistaciaRecorrida;
+
+        public int IDVehiculo { get => _IDVehiculo; set => _IDVehiculo = value; }
+        public int IDCliente { get => _IDCliente; set => _IDCliente = value; }
+        public int IDMarca { get => _IDMarca; set => _IDMarca = value; }
+        public string Modelo { get => _Modelo; set => _Modelo = value; }
+        public string Anno { get => _Anno; set => _Anno = value; }
+        public string VIN { get => _VIN; set => _VIN = value; }
+        public string Placa { get => _Placa; set => _Placa = value; }
+        public bool TipodeDistancia { get => _TipodeDistancia; set => _TipodeDistancia = value; }
+        public float DistaciaRecorrida { get => _DistaciaRecorrida; set => _DistaciaRecorrida = value; }
     }
 }
